83530f26ff117058936f0eb97dccd50259686df4
Rumsse <majafrania@spoko.pl>
2026-03-03 17:10:59 +0100

    Update TunnelMapGenerator.cs

--- Assets/Rumsse/Scripts/TunnelGen/Generation/TunnelMapGenerator.cs
+++ Assets/Rumsse/Scripts/TunnelGen/Generation/TunnelMapGenerator.cs
@@ -1,8 +1,31 @@
-using System;
-using System.Collections.Generic;
+using UnityEngine;
+using TunnelGen.Camera;
+using TunnelGen.Config;
+using TunnelGen.Random;
 
-namespace TunnelGen.Generation {
-    public class TunnelMapGenerator {
-        // Class contents here
-    }
+namespace TunnelGen.Generation
+{
+	public sealed class TunnelMapGenerator : MonoBehaviour
+	{
+		[SerializeField] TunnelGenerationSettings generationSettings;
+		[SerializeField] TunnelPrefabCatalog prefabCatalog;
+		[SerializeField] Transform mapRoot;
+		[SerializeField] PathController pathController;
+
+		IRandomSource randomSource;
+
+		void Awake() => randomSource = generationSettings.CreateRandomSource();
+
+		void Start()
+		{
+			if (!generationSettings || !prefabCatalog || !mapRoot || !pathController)
+				throw new MissingReferenceException($"{nameof(TunnelMapGenerator)} is missing references");
+
+			var buildContext = new TunnelBuildContext(generationSettings, prefabCatalog, mapRoot, randomSource);
+			var buildResult = TunnelLayoutBuilder.Build(buildContext);
+
+			pathController.Initialize(buildResult.PathCentersX, buildResult.DefaultBranchId);
+			pathController.SetTopology(buildResult.TopologyState);
+		}
+	}
 }
